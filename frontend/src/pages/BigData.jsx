de72b7996a8a91e5a8faabec54543e2bf5ddfe2f
vihan.rajpara <vihan.rajpara@safeinvest.co.in>
2025-06-13 11:15:26 +0530

    set up frontend

---
+++ frontend/src/pages/BigData.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box,Button, Typography } from '@mui/material';
+
+const BigData = () => {
+  return (
+    <Box>
+      <Typography variant="h4">Big Data Page</Typography>
+      <Button variant="contained" color="primary">Click Me</Button>
+    </Box>
+  );
+};
+
+export default BigData;
