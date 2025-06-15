6bf60ae6c797e0333976c6257acf574e3d204fb1
vihan.rajpara <vihan.rajpara@safeinvest.co.in>
2025-06-15 14:14:32 +0530

    remove secret

--- backend/src/main/java/com/springbatch/bigdata/config/JdbcManualConfig.java
+++ backend/src/main/java/com/springbatch/bigdata/config/JdbcManualConfig.java
@@ -13,9 +13,7 @@ public class JdbcManualConfig {
 		System.out.println("entering to database connection. ");
 		BasicDataSource dataSource = new BasicDataSource();
 		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://x5EFVVWUw377s7h.root:x0jYtHdDr4EuqG0U@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/springbatch?sslMode=VERIFY_IDENTITY");
-		dataSource.setUsername("x5EFVVWUw377s7h.root");
-		dataSource.setPassword("x0jYtHdDr4EuqG0U");
+
 		dataSource.setInitialSize(2);
 		System.out.println("datasource ::" + dataSource);
 		
